8afb9aaed615adb948f5afd0e641a71745627318
Josh Cheek <josh.cheek@gmail.com>
2011-09-13 17:14:31 -0500

    Update campaign model to accept nested form elements

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,8 +1,11 @@
 class Campaign < ActiveRecord::Base
 
   acts_as_indexed :fields => [:title]
-  has_many :offices
+  has_many :offices, :dependent => :destroy
 
   validates :title, :presence => true, :uniqueness => true
   
+  accepts_nested_attributes_for :offices, 
+      :reject_if => lambda { |attrs| attrs[:title].blank? },
+      :allow_destroy => true
 end
