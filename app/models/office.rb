736fba8890fdd0a6b656453c0f1a52f0af447d27
Josh Cheek <josh.cheek@gmail.com>
2011-09-13 17:15:53 -0500

    Add office models

--- app/models/office.rb
+++ app/models/office.rb
@@ -1,9 +1,13 @@
 class Office < ActiveRecord::Base
 
   acts_as_indexed :fields => [:title]
-  has_many :candidates
+  has_many :candidates, :dependent => :destroy
   belongs_to :campaign
   
   validates :title, :presence => true
+  
+  accepts_nested_attributes_for :candidates,
+      :reject_if => lambda { |attrs| attrs[:title].blank? },
+      :allow_destroy => true
   
 end
