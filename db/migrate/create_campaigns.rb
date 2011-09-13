10b4829f4144fc260be4b1d8859004af73326954
Josh Cheek <josh.cheek@gmail.com>
2011-09-13 11:13:24 -0500

    Add offices and candidates to campaign

--- db/migrate/create_campaigns.rb
+++ db/migrate/create_campaigns.rb
@@ -2,27 +2,46 @@ class CreateCampaigns < ActiveRecord::Migration
 
   def self.up
     create_table :campaigns do |t|
-      t.string :title
-      t.integer :position
-
+      t.string      :title
+      t.integer     :position
+      t.timestamps
+    end
+    
+    create_table :offices do |t|
+      t.string      :title
+      t.references  :campaign
+      t.integer     :position
       t.timestamps
     end
 
-    add_index :campaigns, :id
+    create_table :candidates do |t|
+      t.string      :name
+      t.references  :office
+      t.integer     :position
+      t.timestamps
+    end
+
+
+    add_index :campaigns  , :id
+    add_index :offices    , :id
+    add_index :candidates , :id
 
     load(Rails.root.join('db', 'seeds', 'campaigns.rb'))
   end
 
   def self.down
-    if defined?(UserPlugin)
-      UserPlugin.destroy_all({:name => "campaigns"})
-    end
+    [:campaigns, :offices, :candidates].each do |table_name|
+      if defined?(UserPlugin)
+        UserPlugin.destroy_all :name => table_name.to_s
+      end
 
-    if defined?(Page)
-      Page.delete_all({:link_url => "/campaigns"})
+      if defined?(Page)
+        Page.delete_all :link_url => "/#{table_name}"
+      end
+      
+      drop_table table_name
     end
-
-    drop_table :campaigns
   end
 
 end
+
