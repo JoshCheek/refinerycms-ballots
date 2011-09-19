31d831b4b73b24a03c5728789f75a371e8206ef6
Josh Cheek <josh.cheek@gmail.com>
2011-09-19 12:21:43 -0500

    Enable voting

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,26 +4,21 @@ class VotesController < ApplicationController
   before_filter :find_page
 
   def create
-    present(@page)
+    @ballot_vote = BallotVote.new params[:ballot_vote]
+    @ballot_vote.ballot = @ballot
+    if @ballot_vote.save
+      present @page
+    else
+      render :new
+    end
   end
 
   def new
-    @ballot_vote = ballot_vote_for @ballot
+    @ballot_vote = BallotVote.new_for_ballot @ballot
     present(@page)
   end
-  
+    
 protected
-
-  def ballot_vote_for(ballot)
-    bv = BallotVote.new :ballot => ballot
-    ballot.offices.each do |office|
-      ov = bv.office_votes.build :office => office
-      office.candidates.each do |candidate|
-        ov.candidate_votes.build :candidate => candidate
-      end
-    end
-    bv
-  end
 
   def find_ballot
     @ballot = Ballot.where(:id => params[:ballot_id]).first
