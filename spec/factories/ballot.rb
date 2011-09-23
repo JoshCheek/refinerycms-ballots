b2ea89bd9d571e0248b80067c8d3d78f7d35533b
Josh Cheek <josh.cheek@gmail.com>
2011-09-22 22:39:56 -0500

    Move the big ad-hoc factory method into a factory method

--- spec/factories/ballot.rb
+++ spec/factories/ballot.rb
@@ -15,3 +15,26 @@ Factory.define :ballot do |ballot|
   ballot.start_date          { 10.days.ago }
   ballot.end_date            { 10.days.from_now }
 end
+
+def Factory.create_whole_ballot
+  Ballot.create Factory.attributes_for(:ballot, :title=>"2012 election",
+                 "offices_attributes"=>
+                  {"0"=>
+                    Factory.attributes_for(:office, :title=>"President", :number_of_positions=>1,
+                                                     "candidates_attributes"=>
+                                                      {"0"=>Factory.attributes_for(:candidate, :name=>"Bradford"),
+                                                       "1316013000645"=>Factory.attributes_for(:candidate, :name=>"Gary")}),
+                   "1316013014442"=>
+                    Factory.attributes_for(:office, :title=>"Vice President", :number_of_positions=>1,
+                                                     "candidates_attributes"=>
+                                                      {"0"=>Factory.attributes_for(:candidate, :name=>"Jim"),
+                                                       "1316013025758"=>Factory.attributes_for(:candidate, :name=>"Cara")}),
+                   "1316013254746"=>
+                    Factory.attributes_for(:office, :title=>"Board of Directors", :number_of_positions=>2,
+                                                     "candidates_attributes"=>
+                                                      {"0"=>Factory.attributes_for(:candidate, :name=>"Joe"),
+                                                       "1316013264883"=>Factory.attributes_for(:candidate, :name=>"Bob"),
+                                                       "1316013267758"=>Factory.attributes_for(:candidate, :name=>"Pete"),
+                                                       "1316013271569"=>Factory.attributes_for(:candidate, :name=>"Uma")})})
+  Ballot.last
+end
--- spec/models/voting_spec.rb
+++ spec/models/voting_spec.rb
@@ -1,45 +1,23 @@
 require 'spec_helper'
+require 'spec/factories/all'
 
 describe 'Voting' do
   
   # ensure db is set up right
   before :each do
     [Ballot, Office, Candidate, Member].each(&:delete_all)
-    Ballot.create  "title"=>"2012 election",
-                   "start_date"=>10.days.ago,
-                   "end_date"=>10.days.from_now,
-                   "offices_attributes"=>
-                    {"0"=>
-                      {"_destroy"=>"false",
-                       "title"=>"President",
-                       "number_of_positions"=>"1",
-                       "candidates_attributes"=>
-                        {"0"=>{"name"=>"Bradford", "_destroy"=>"false"},
-                         "1316013000645"=>{"name"=>"Gary", "_destroy"=>"false"}}},
-                     "1316013014442"=>
-                      {"_destroy"=>"false",
-                       "title"=>"Vice President",
-                       "number_of_positions"=>"1",
-                       "candidates_attributes"=>
-                        {"0"=>{"name"=>"Jim", "_destroy"=>"false"},
-                         "1316013025758"=>{"name"=>"Cara", "_destroy"=>"false"}}},
-                     "1316013254746"=>
-                      {"_destroy"=>"false",
-                       "title"=>"Board of Directors",
-                       "number_of_positions"=>"2",
-                       "candidates_attributes"=>
-                        {"0"=>{"name"=>"Joe", "_destroy"=>"false"},
-                         "1316013264883"=>{"name"=>"Bob", "_destroy"=>"false"},
-                         "1316013267758"=>{"name"=>"Pete", "_destroy"=>"false"},
-                         "1316013271569"=>{"name"=>"Uma", "_destroy"=>"false"}}}}
+    Factory.create_whole_ballot
+    
+    # just to make sure it does what we think it should
     Ballot.count.should == 1
     Office.count.should == 3
     Candidate.count.should == 8
     Member.count.should == 0
   end
   
-  let(:member)    { Member.create! :unique_identifier => 'A123'; Member.last }
-  let(:bv)        { BallotVote.new_for_ballot Ballot.first }
+  let(:ballot)    { Ballot.first}
+  let(:bv)        { BallotVote.new_for_ballot ballot }
+  let(:member)    { get_member! :unique_identifier => 'A123' }
   let(:president) { bv.office_votes.to_a.find { |ov| ov.office.title == 'President' } }
   let(:bradford)  { president.candidate_votes.to_a.find { |cv| cv.candidate.name == 'Bradford' } }
   let(:gary)      { president.candidate_votes.to_a.find { |cv| cv.candidate.name == 'Gary' } }
