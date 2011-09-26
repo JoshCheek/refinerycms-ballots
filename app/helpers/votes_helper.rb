797ac1a8ec8e5ac7c411af82d6b4897ce4ab09d5
Josh Cheek <josh.cheek@gmail.com>
2011-09-26 10:03:17 -0500

    Place error messages next to forms that are invalid

--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -8,4 +8,13 @@ module VotesHelper
   def undervoted?(office_vote)
     office_vote.num_votes < office_vote.max_num_votes
   end
+  
+  def show_errors_for(office)
+    return '' if office.errors.empty?
+    raw "<div class='errorExplanation'>#{errors_as_li office}</div>"
+  end
+  
+  def errors_as_li(office)
+    office.errors.full_messages.map { |msg| "<li>#{msg}</li>" }.join
+  end
 end
